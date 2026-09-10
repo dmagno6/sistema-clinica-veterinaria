79dadb09cfbc3f3f4c6d359c6220e2378849b44e
Gabriel Rosa <167138044+G4BR13l-R0S4@users.noreply.github.com>
2026-09-09 21:40:53 -0300

    Remoção de Informações inexistentes

--- scripts/V12__insert_into_atendente.sql
+++ scripts/V12__insert_into_atendente.sql
@@ -1,3 +1,3 @@
 INSERT INTO atendente (nome, cpf) VALUES
-    ('Pedro Henrique Souza', '666.666.666-66', NULL),
-    ('Fernanda Ramos', '333.333.333-33', 3);
+    ('Pedro Henrique Souza', '666.666.666-66'),
+    ('Fernanda Ramos', '333.333.333-33');
