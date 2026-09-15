60df1f215896b18314b663f8260d6a15ea17fc47
Kaio de Lima <sanduiche152@gmail.com>
2026-09-15 17:21:23 -0400

    Create V19__insert_into_item_atendimento_2.sql

---
+++ scripts/V19__insert_into_item_atendimento_2.sql
@@ -0,0 +1,3 @@
+-- Primeira linha: Thor com Juliana, Nenhum Produto, Consulta, 1, 80.00
+INSERT INTO item_atendimento (id_atendimento, id_produto, id_servico, quantidade, valor) VALUES 
+(1, NULL, 1, 1, 80.00);
